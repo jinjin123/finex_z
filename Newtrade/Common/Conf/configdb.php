cbaf262454a91242d36ebc293daf0e5b8a0395b0
Jinjin123 <hanzijinjin123@gmail.com>
2020-08-24 17:56:07 +0800

    Push back from production

--- Newtrade/Common/Conf/configdb.php
+++ Newtrade/Common/Conf/configdb.php
@@ -2,7 +2,7 @@
 return array(
     /* 数据库设置 */
     'DB_TYPE' => 'mysql', // 数据库类型
-    'DB_HOST' => '8.210.69.71', // 服务器地址
+    'DB_HOST' => '127.0.0.1', // 服务器地址
     'DB_NAME' => 'target', // 数据库名
     'DB_USER' => 'root', // 用户名
     'DB_PWD' => 'god123456', // 密码
